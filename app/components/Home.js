c157324d9901ca2259f545935fbd66841b44a0e0
Ken Kudo <kekudo@ucsd.edu>
2017-05-21 23:46:12 -0700

    got map to display; working on adding heatmap layer now

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,3 +1,6 @@
+import { render } from 'react-dom';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import HeatmapLayer from '../../react-leaflet-heatmap-layer/src/HeatmapLayer';
 var React = require('react');
 var Nav = require('./Nav');
 
@@ -11,13 +14,26 @@ class Home extends React.Component {
   }
 
   render() {
+
+    const position = [27.980626, 84.578747];
+
     return (
       <div className = 'home-container'>
         <Nav
           selected = {0}
           history={this.props.history}
         />
-        <p>[Heat Map goes here]</p>
+        <Map id="mapid" center={position} zoom={7}>
+          <TileLayer
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={position}>
+             <Popup>
+               <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+             </Popup>
+          </Marker>
+        </Map>
       </div>
     )
   }
