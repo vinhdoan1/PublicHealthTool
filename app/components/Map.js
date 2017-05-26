1d7bb1be84da343d7f3f435512b1769ba91e4d78
Ken Kudo <kekudo@ucsd.edu>
2017-05-26 13:06:49 -0700

    work in progress heatmap

---
+++ app/components/Map.js
@@ -0,0 +1,10 @@
+import HeatmapLayer from 'react-leaflet-heatmap-layer';
+
+export default class PH_HeatMapLayer extends HeatmapLayer {
+
+    constructor(props) {
+        super(props);
+    }
+
+    createLeafletElement () {}
+}
